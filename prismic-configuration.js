71e9926f3c4f83126e6e35f120997500d59a43be
Manuel Ortega Carcamo <mortega@apgca.cl>
2021-04-28 16:57:56 -0400

    initialize repository

---
+++ prismic-configuration.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  
+  // The repo name for your Prismic repository (found in the url of your repo: https://manu-test.prismic.io/)
+  prismicRepo: 'manu-test',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+
+  // Language configuration in your Prismic repository: Settings / Translations & locals
+  defaultLanguage: 'en-us',
+  langs: ['en-us', 'fr-fr'],
+}
